c073f73c1d026f287017b705df8695a5861fc9f9
Ben Onoja <benonoja678@gmail.com>
2024-12-02 20:04:40 +0100

    updated index.js

--- index.js
+++ index.js
@@ -26,8 +26,7 @@ const corsOptions = {
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'telegram-user-id'], // Allow telegram-user-id in headers
 };
-app.options('*',cors());
-
+app.use(cors(corsOptions));
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
